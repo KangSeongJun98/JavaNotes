1cfbdca302a6079bd98c0145773bac1b711d6cea
KangSeongJun98 <156163532+KangSeongJun98@users.noreply.github.com>
2024-01-10 16:55:02 +0900

    Add files via upload
    자바 시작

---
+++ ch01_start/Hello.java
@@ -0,0 +1,11 @@
+package ch01_start;
+
+public class Hello {
+	// 라인 주석
+	/* 전체 주석 */
+	/* alt + 위 아래  (라인이동)
+	   ctrl + d       (라인삭제) */
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+	}
+}
