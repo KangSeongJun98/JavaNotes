c9eaf772cf5874aebb40c76241d107fbfa5e20ea
KangSeongJun98 <156163532+KangSeongJun98@users.noreply.github.com>
2024-04-12 17:32:11 +0900

    Delete NLP directory

--- NLP/word2vec02.py
+++
@@ -1,5 +0,0 @@
-from gensim.models import FastText
-from gensim.models import Word2Vec
-
-f_model = FastText.load('./movie_emb_f.model')
-print(f_model.wv.most_similar(positive=['재미','관상'], negative=['노잼']))
